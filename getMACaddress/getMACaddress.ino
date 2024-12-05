cfe94abd86ef451e8eff0356289e2645ff20b7eb
Scott Fitzgerald <scott@droolcup.com>
2024-12-05 15:37:12 -0500

    Update getMACaddress.ino

--- getMACaddress/getMACaddress.ino
+++ getMACaddress/getMACaddress.ino
@@ -1,27 +1,16 @@
 #include <WiFi.h>
 
-char ssid[] = "yourNetwork";  // bogus name just to start the library
-
-byte mac[6];  // the MAC address of your board
-
 void setup() {
-  Serial.begin(9600);
-  while(!Serial){};
-  WiFi.begin(ssid);
 
-  WiFi.macAddress(mac);
-  Serial.print("MAC: ");
-  Serial.print(mac[5], HEX);
-  Serial.print(":");
-  Serial.print(mac[4], HEX);
-  Serial.print(":");
-  Serial.print(mac[3], HEX);
-  Serial.print(":");
-  Serial.print(mac[2], HEX);
-  Serial.print(":");
-  Serial.print(mac[1], HEX);
-  Serial.print(":");
-  Serial.println(mac[0], HEX);
+  Serial.begin(115200);
+  while (!Serial) {};
+
+  String mac = WiFi.macAddress();
+
+  // Print the MAC address to the Serial Monitor
+  Serial.print("MAC Address: ");
+  Serial.println(mac);
 }
 
-void loop() {}
+void loop() {
+}
