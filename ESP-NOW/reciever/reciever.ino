17332b545bd804de922319ee05f6fa254c734554
Scott Fitzgerald <scott@droolcup.com>
2024-12-05 15:59:01 -0500

    Create reciever.ino

---
+++ ESP-NOW/reciever/reciever.ino
@@ -0,0 +1,36 @@
+#include <esp_now.h>
+#include <WiFi.h>
+
+// variable to hold data from sender
+int incoming = 0;
+
+// callback function that will be executed when data is received
+void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
+  memcpy(&incoming, incomingData, sizeof(incoming));
+  Serial.print("Bytes received: ");
+  Serial.println(len);
+  Serial.print("value: ");
+  Serial.println(incoming);
+}
+ 
+void setup() {
+  // Initialize Serial Monitor
+  Serial.begin(115200);
+  while(!Serial){};
+  // Set device as a Wi-Fi Station
+  WiFi.mode(WIFI_STA);
+
+  // Init ESP-NOW
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+  
+  // Once ESPNow is successfully Init, we will register for recv CB to
+  // get recv packer info
+  esp_now_register_recv_cb(esp_now_recv_cb_t(OnDataRecv));
+}
+ 
+void loop() {
+
+}
